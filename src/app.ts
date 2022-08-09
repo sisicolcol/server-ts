07d288fa238534ee6545a8ff6027651ed0d0165c
Seonguk <xsungwook3x@naver.com>
2022-08-09 16:10:33 +0900

    Add: ts 방식을 위한 src폴도로 다시 재배치 및 dist폴더 설정 완료

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import {Request,Response,NextFunction} from "express";
+let express=require('express');
+let app= express();
+
+app.get('/',(req:Request,res:Response,next:NextFunction) =>{
+    res.send('express server start!');
+});
+
+app.listen(4000,()=>{
+    console.log(`
+    #############################################
+        🛡️ Server listening on port: 4000 🛡️
+    #############################################    
+    `)
+})
