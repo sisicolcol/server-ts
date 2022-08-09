4828020e34972c3bea12278f2c724eece58481a4
Seonguk <xsungwook3x@naver.com>
2022-08-10 00:40:17 +0900

    Add: start setting sequelize with RDS and it's worked

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,33 @@
 import {Request,Response,NextFunction} from "express";
+import * as bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import {sequelize} from "./models";
+
+dotenv.config();
+
 let express=require('express');
-let app= express();
+const app= express();
+const router=express.Router();
+
+app.use(bodyParser.json())
+app.use(express.json());
 
 app.get('/',(req:Request,res:Response,next:NextFunction) =>{
     res.send('express server start!');
 });
 
-app.listen(4000,()=>{
+app.listen(4000,async ()=>{
     console.log(`
     #############################################
         🛡️ Server listening on port: 4000 🛡️
     #############################################    
     `)
+
+    await sequelize.authenticate()
+        .then(async () => {
+            console.log("connection success");
+        })
+        .catch((e)=>{
+            console.log('TT : ' ,e);
+        })
 })
